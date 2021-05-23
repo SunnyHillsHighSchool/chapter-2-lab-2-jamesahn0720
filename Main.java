91ffc57b2a051aca9442f9908781ddacdd046277
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-05-22 20:42:01 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,9 +6,19 @@ class Main
 	{
 		ArrayList<String> names = new ArrayList<String>();
 		//write code below
-		
+		//write code below
+		names.add("Hello");
+    names.add("Hi");
+    names.add("What");
+    names.add("Is");
+    names.add("Your");
+    names.add("Name");
 		
 		//leave below code alone
 		return names;
 	}
+	public static void main(String[] args)
+  {
+    System.out.println(test());
+  }
 }
